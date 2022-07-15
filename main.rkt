7041ebc109f25155d332bc9c5efb106950fb5e67
Mike Delmonaco <mthundershock@gmail.com>
2022-07-15 16:21:09 -0400

    list schema

--- main.rkt
+++ main.rkt
@@ -7,12 +7,15 @@
 
 #|
 <racket-def> := ... | (define-schema <schema-id> <schema>)        interface macro
-<racket-expr> := ... | (validate <schema-id> <racket-expr>)       interface macro
+<racket-expr> := ... | (validate-json <schema-id> <racket-expr>)  interface macro
 <schema-id> := <identifier>
 <schema> := string | number | boolean | null                      atomic types
           | <schema-id>                                           schema reference
-          | (list-of <schema>)                                    list schema
+          | (list-of <schema>)                                    list-of schema
           | (object <field> ...)                                  object schema
+          | (? <expr>)                                            predicate schema
+          | (and <schema> <schema>)                               and schema
+          | (list <schema> ...)                                   list schema
 <field> := (<identifier> <schema>)                                object field
 |#
 
@@ -40,7 +43,7 @@
 
 (begin-for-syntax
   (define-syntax-class core-schema
-    #:datum-literals (string number boolean null list-of object ? and)
+    #:datum-literals (string number boolean null list-of object ? and list)
     #:description "core schema"
     (pattern string)
     (pattern number)
@@ -50,6 +53,7 @@
     (pattern (object field:core-field ...))
     (pattern (? test:expr))
     (pattern (and schema1:core-schema schema2:core-schema))
+    (pattern (list schema:core-schema ...))
     (pattern name:id))
 
   (define-syntax-class core-field
@@ -99,10 +103,16 @@
   (void (schema1 json))
   (schema2 json))
 
+(define ((#%list-schema schemas) json)
+  (if (and (list? json) (= (length schemas) (length json)))
+      (for/list ([schema schemas] [element json])
+        (schema element))
+      (error (format "expected a list with ~a elements" json))))
+
 (define-syntax (core-schema->racket stx)
   (syntax-parse stx
     ; I don't want to repeat myself, but a literal set didn't seem to work
-    #:datum-literals (string number boolean null list-of object/schema ? and)
+    #:datum-literals (string number boolean null list-of object/schema ? and list)
     [(_ number) #'#%number-schema]
     [(_ boolean) #'#%boolean-schema]
     [(_ string) #'#%string-schema]
@@ -111,6 +121,7 @@
     [(_ (object (field value-schema:core-schema) ...)) #'(#%object-schema (list (list 'field (core-schema->racket value-schema)) ...))]
     [(_ (? test:expr)) #'(#%?-schema test)]
     [(_ (and schema1:core-schema schema2:core-schema)) #'(#%and-schema (core-schema->racket schema1) (core-schema->racket schema2))]
+    [(_ (list schema:core-schema ...)) #'(#%list-schema (list (core-schema->racket schema) ...))]
     [(_ name:id) #'name]))
 
 ;; interface macros
@@ -140,6 +151,7 @@
   (check-equal? (validate-jsexpr (and (? (const #t)) complex) (hasheq 'im 1 'rl 2)) (hasheq 'im 1 'rl 2))
   (define-schema rose-tree (list-of rose-tree))
   (check-equal? (validate-jsexpr rose-tree '(() () ((())))) '(() () ((()))))
+  (check-equal? (validate-jsexpr (list number boolean) (list 1 #t)) (list 1 #t))
   (check-exn exn:fail? (λ () ((core-schema->racket number) #t)))
   (check-exn exn:fail? (λ () ((core-schema->racket (list-of number)) #t)))
   (check-exn exn:fail? (λ () ((core-schema->racket (list-of number)) (list 1 2 #t))))
@@ -147,7 +159,13 @@
   (check-exn exn:fail? (λ () ((core-schema->racket (and (? even?) (? (const #t)))) 1)))
   (check-exn exn:fail? (λ () ((core-schema->racket (and (? (const #t)) (? even?))) 1)))
   (check-exn exn:fail? (λ () ((core-schema->racket (and (? even?) (? even?))) 1)))
-  (check-exn exn:fail? (λ () (validate-jsexpr rose-tree '(() () (((1))))))))
+  (check-exn exn:fail? (λ () (validate-jsexpr rose-tree '(() () (((1)))))))
+  (check-exn exn:fail? (λ () (validate-jsexpr (list number boolean) 1)))
+  (check-exn exn:fail? (λ () (validate-jsexpr (list number boolean) '())))
+  (check-exn exn:fail? (λ () (validate-jsexpr (list number boolean) '(1 2))))
+  (check-exn exn:fail? (λ () (validate-jsexpr (list number boolean) '(#t #t))))
+  (check-exn exn:fail? (λ () (validate-jsexpr (list number boolean) '(1 #t 3))))
+  (check-exn exn:fail? (λ () (validate-jsexpr (list number boolean) '(#t 1)))))
 
 (define-schema rose-tree (list-of rose-tree))
 
