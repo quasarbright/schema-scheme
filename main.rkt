9749cdf65b0a99442a108df15e16e0a9e7f38e16
Mike Delmonaco <mthundershock@gmail.com>
2022-07-14 23:37:04 -0400

    clean and comment

--- main.rkt
+++ main.rkt
@@ -1,29 +1,9 @@
 #lang racket
 
+(require (for-syntax syntax/parse))
+
 (module+ test
   (require rackunit))
-
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;; For your convenience, we have included LICENSE-MIT and LICENSE-APACHE files.
-;; If you would prefer to use a different license, replace those files with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
-
-;; Code here
 
 #|
 <racket-def> := ... | (define-schema <schema-id> <schema>)        interface macro
@@ -35,7 +15,11 @@
           | (object <field> ...)                                  object schema
 <field> := (<identifier> <schema>)                                object field
 |#
-(require (for-syntax syntax/parse))
+
+; a compiled schema is a
+#;(-> jsexpr? any)
+; returns the result of validating, usually just the json
+; or throws an error
 
 #;(define-literal-forms schema-literals "schema literal"
     (string number boolean null list-of object))
