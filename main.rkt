50b7ebcfec0c65f7815f18b53eeadda7c851d4e0
Mike Delmonaco <mthundershock@gmail.com>
2022-07-16 15:28:14 -0400

    update grammar

--- main.rkt
+++ main.rkt
@@ -12,15 +12,15 @@
 
 #|
 <racket-def> := ... | (define-schema <schema-id> <schema>)        interface macro
-<racket-expr> := ... | (validate-json <schema-id> <racket-expr>)  interface macro
+<racket-expr> := ... | (validate-json <schema> <racket-expr>)     interface macro
 <schema-id> := <identifier>
 <schema> := string | number | boolean | null                      atomic types
           | <schema-id>                                           schema reference
           | (list-of <schema>)                                    list-of schema
-          | (object <field> ...)                                  object schema
-          | (? <expr>)                                            predicate schema
-          | (and <schema> <schema>)                               and schema
           | (list <schema> ...)                                   list schema
+          | (object <field> ...)                                  object schema
+          | (? <expr> <schema> ...)                               predicate schema
+          | (and <schema> ...)                                    and schema
 <field> := (<identifier> <schema>)                                object field
 |#
 
