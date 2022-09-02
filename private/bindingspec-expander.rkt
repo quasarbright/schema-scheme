bf1401764a696ad42f8b4771223fc13a150467b1
Mike Delmonaco <mthundershock@gmail.com>
2022-09-02 02:03:32 -0400

    macro schemas, cons, list, quasiquote, more tests

--- private/bindingspec-expander.rkt
+++ private/bindingspec-expander.rkt
@@ -1,7 +1,8 @@
 #lang racket
 
 (module+ test (require rackunit))
-(provide (all-defined-out))
+(provide (all-defined-out)
+         (for-space schema-scheme (all-defined-out)))
 
 (require bindingspec (for-syntax syntax/parse))
 
@@ -23,6 +24,7 @@
   (nesting-nonterminal schema (tail)
                        #:description "schema"
                        #:allow-extension schema-macro
+                       #:binding-space schema-scheme
                        any
                        #:binding {tail}
                        v:schema-ref
@@ -36,10 +38,14 @@
                        ; for now, => and 'when' have their own scopes.
                        ; TODO ask michael about it
                        (when s:schema guard:expr)
-                       #:binding [(nest-one s (host guard)) (nest-one s tail)]
+                       #:binding (nest-one s (host guard))
                        ;#:binding [(nest-one s (host guard)) (nest-one s tail)]
                        (object-has-field name:id s:schema)
-                       #:binding (nest-one s tail)))
+                       #:binding (nest-one s tail)
+                       (cons car-schema:schema cdr-schema:schema)
+                       #:binding (nest-one car-schema (nest-one cdr-schema tail))
+                       (? valid?:expr desc:expr)
+                       #:binding [(host valid?) (host desc) tail]))
 
 (define-syntax define-schema-scheme-syntax
   (syntax-parser
@@ -52,6 +58,17 @@
     [(_ (macro-name:id args ...) body ...) #'(define-schema-syntax macro-name (λ (args ...) body ...))]
     [(_ macro-name:id transformer) #'(define-schema-scheme-syntax macro-name (schema-macro transformer))]))
 
+; TODO this duplicates the value expression, but I'm not sure if it's possible to avoid and have a good description
+(define-schema-syntax equal? (syntax-rules () [(equal? val) (? (λ (json) (equal? json val)) val)]))
+(define-schema-syntax quote (syntax-rules  () [(quote datum) (equal? (quote datum))]))
+(define-schema-syntax list (syntax-rules () [(list) '()] [(list p0 p ...) (cons p0 (list p ...))]))
+; simple quasiquote schema
+; max depth of 1, no splicing unquote, only supports list quasi-schemas
+(define-schema-syntax quasiquote (syntax-rules (unquote)
+                                   [(quasiquote (unquote schema)) schema]
+                                   [(quasiquote (schema ...)) (list (quasiquote schema)  ...)]
+                                   [(quasiquote datum) 'datum]))
+
 (define-host-interface/expression (validate-json s:schema-top json:expr)
   #:binding [s (host json)]
   #:with s^ (compile-schema #'s)
@@ -59,7 +76,7 @@
 
 (begin-for-syntax
   (define-literal-set schema-literals
-    #:datum-literals (list-of list and or bind when quote quasiquote unquote => cons object-has-field any equal?)
+    #:datum-literals (list-of list and or bind when quote quasiquote unquote => cons object-has-field any equal? ?)
     ())
 
   (define (compile-schema schema)
@@ -75,7 +92,6 @@
          #:literal-sets (schema-literals)
          [(bind var:id schema)
           #:with var^ (compile-binder! #'var)
-          (displayln #'var^)
           (compile-validate #'schema #'json #'result #'(let ([var^ result]) body) #'on-fail)]
          [(when schema guard)
           (compile-validate #'schema
@@ -91,10 +107,37 @@
                             #'on-fail)]
          [any #'(let ([result json]) body)]
          [(object-has-field name:id schema)
+          ; this shouldn't be necessary, but I'm paranoid bc this compiler doesn't get intro scopes
+          #:with (field) (generate-temporaries (list #'field))
           #`(if (and (hash? json) (hash-has-key? json 'name))
                 (let ([field (hash-ref json 'name)])
                   #,(compile-validate #'schema #'field #'result #'body #'on-fail))
-                (on-fail (format "expected an object, but got ~a" json)))])]))
+                (on-fail (format "expected an object, but got ~a" json)))]
+         [(cons car-schema cdr-schema)
+          ; since this compiler isn't a macro, we don't get intro scopes!
+          ; this causes introduced variables of nested conses to shadow each other,
+          ; so we need to generate temporaries to avoid shadowing.
+          #:with (car-value cdr-value) (generate-temporaries (list #'car-value #'cdr-value))
+          #:with (car-result cdr-result) (generate-temporaries (list #'car-result #'cdr-result))
+          #`(if (cons? json)
+                (let ([car-value (car json)]
+                      [cdr-value (cdr json)])
+                  #,(compile-validate #'car-schema
+                                      #'car-value
+                                      #'car-result
+                                      (compile-validate #'cdr-schema
+                                                        #'cdr-value
+                                                        #'cdr-result
+                                                        #'(let ([result (cons car-result cdr-result)])
+                                                            body)
+                                                        #'on-fail)
+                                      #'on-fail))
+                (on-fail (format "expected a cons, but got ~a " json)))]
+         ; TODO 'any' should be written in terms of this
+         [(? valid? desc)
+          #`(if (#,(compile-host-expr #'valid?) json)
+                (let ([result json]) body)
+                (on-fail "expected ~a, but got ~a" #,(compile-host-expr #'desc) json))])]))
 
   (define (compile-host-expr e)
     (resume-host-expansion e #:reference-compilers ([var compile-reference]))))
@@ -105,4 +148,16 @@
   (check-equal? (validate-json (=> any 2) 1) 2)
   (check-equal? (validate-json (when any #t) 1) 1)
   (check-equal? (validate-json (bind x any) 1) 1)
-  (check-equal? (validate-json (=> (bind y any) (list y)) 1) '(1)))
+  (check-equal? (validate-json (=> (bind y any) (list y)) 1) '(1))
+  (check-equal? (validate-json (when (bind y any) (even? y)) 2) 2)
+  (check-equal? (validate-json (cons any any) '(1 2)) '(1 2))
+  (check-equal? (validate-json (=> (cons (bind x any) (bind y any)) (list x y)) '(1 2)) '(1 (2)))
+  (check-equal? (validate-json (? even? "an even number") 2) 2)
+  (check-equal? (validate-json (equal? 2) 2) 2)
+  (check-equal? (validate-json '2 2) 2)
+  ; regression test: outer car-result would get shadowed by inner car-result bc no intro scopes.
+  ; used to result in '(2 2)
+  (check-equal? (validate-json (cons any (cons any any)) '(1 2)) '(1 2))
+  (check-equal? (validate-json (list any any) '(1 2)) '(1 2))
+  (check-equal? (validate-json (=> (list (bind a any) (bind b any)) (list b a)) '(1 2)) '(2 1))
+  (check-equal? (validate-json (=> `(1 2 ,(bind x any)) x) '(1 2 3)) 3))
